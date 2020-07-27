8502f2f39d94a87b3006e8e463f2bb8a6eceb966
Robert Pirtle <astropirtle@gmail.com>
2020-07-27 10:48:53 -0700

    fix linting errors

--- src/handler.ts
+++ src/handler.ts
@@ -3,11 +3,11 @@ import { runScraper } from '@services/scrape';
 import 'source-map-support/register';
 
 export const orestarScraper: APIGatewayProxyHandler = async () => {
-  const stuff = await runScraper()
+  const stuff = await runScraper();
   return {
     statusCode: 200,
     body: JSON.stringify({
-      headers: stuff
-    }, null, 2)
-  }
-}
+      headers: stuff,
+    }, null, 2),
+  };
+};
