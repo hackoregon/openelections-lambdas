de3c835c96694a2e2140a209cf9f74f075c37d35
Andrew <andyerickson2014@gmail.com>
2020-07-26 20:03:29 -0700

    Initial commit

---
+++ src/handler.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { runScraper } from '@services/scrape';
+import 'source-map-support/register';
+
+export const orestarScraper: APIGatewayProxyHandler = async () => {
+  const stuff = await runScraper()
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      headers: stuff
+    }, null, 2)
+  }
+}
