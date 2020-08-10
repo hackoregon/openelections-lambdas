14f278c23e97c22b536bc0197fa757908b983bd1
Andrew <andyerickson2014@gmail.com>
2020-08-09 21:31:50 -0700

    wip add db connection

---
+++ controller/contributions.ts
@@ -0,0 +1,20 @@
+import db from "@models/db";
+import { Contribution } from "@models/entity/Contribution";
+
+export async function addContributions (contributions: Contribution[]): Promise<void> {
+  try {
+    const connection = await db();
+    const contributionRepository = connection.getRepository(Contribution)
+    await Promise.all(contributions.map( async (contribution: Contribution) => {
+      const oaeContribution = new Contribution()
+      oaeContribution = {
+        ...contribution
+      }
+      if (await oaeContribution.isValidAsync()) {
+        contributionRepository.update()
+      }
+    }))
+  } catch (error) {
+    
+  }
+}
