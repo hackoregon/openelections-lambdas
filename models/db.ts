16a3075d7a41d635cbaf13a2f9eada7805aa0d34
Andrew <andyerickson2014@gmail.com>
2020-08-31 20:14:44 -0700

    skip using existing connection

--- models/db.ts
+++ models/db.ts
@@ -1,16 +1,12 @@
 import 'reflect-metadata';
 import {
-  createConnection, Connection, getConnection, getConnectionManager,
+  createConnection, Connection, getConnection
 } from 'typeorm';
 import ORMConfig from './ormConfig';
 import { reportError } from '@services/bugSnag';
 
 export default async (): Promise<Connection> => {
   try {
-    const connectionManager = getConnectionManager();
-    if (connectionManager.has('default')) {
-      return await connectionManager.get('default');
-    }
     const connection: Connection = await createConnection(ORMConfig);
     return connection;
   } catch (error) {
